162b85c72dc0efe853a1ba1f198c0eda600a62bf
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-08-24 12:31:09 -0500

    Challenge 9 start

--- app.py
+++ app.py
@@ -26,62 +26,62 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-# @app.route("/")
+@app.route("/")
 
-# def welcome():
-#     return(
-#     '''
-#     Welcome to the Climate Analysis API!
-#     Available Routes:
-#     /api/v1.0/precipitation
-#     /api/v1.0/stations
-#     /api/v1.0/tobs
-#     /api/v1.0/temp/start/end
-#     ''')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
-# @app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/precipitation")
 
-# def precipitation():
-#    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-#    precipitation = session.query(Measurement.date, Measurement.prcp).\
-#     filter(Measurement.date >= prev_year).all()
-#    precip = {date: prcp for date, prcp in precipitation}
-#    return jsonify(precip)
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
 
-# @app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/stations")
 
-# def stations():
-#     results = session.query(Station.station).all()
-#     stations = list(np.ravel(results))
-#     return jsonify(stations=stations)
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
-# @app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/tobs")
 
-# def temp_monthly():
-#     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-#     results = session.query(Measurement.tobs).\
-#       filter(Measurement.station == 'USC00519281').\
-#       filter(Measurement.date >= prev_year).all()
-#     temps = list(np.ravel(results))
-#     return jsonify(temps=temps)
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
-# @app.route("/api/v1.0/temp/<start>")
-# @app.route("/api/v1.0/temp/<start>/<end>")
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
 
-# def stats(start=None, end=None):
-#     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-#     if not end:
-#         results = session.query(*sel).\
-#             filter(Measurement.date >= start).all()
-#         temps = list(np.ravel(results))
-#         return jsonify(temps)
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
 
-#     results = session.query(*sel).\
-#         filter(Measurement.date >= start).\
-#         filter(Measurement.date <= end).all()
-#     temps = list(np.ravel(results))
-#     return jsonify(temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
